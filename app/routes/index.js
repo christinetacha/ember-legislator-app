f670218a45da5d888be90e3de5af3be3bc76640d
christine <tacha.christine@gmail.com>
2016-08-23 09:24:49 -0700

    add functioning API request, and successfully display results on index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend ({
+
+export default Ember.Route.extend({
   actions: {
     zipLookup(params) {
       this.transitionTo('results', params.zip);
--- app/routes/results.js
+++ app/routes/results.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend ({
-  model: function(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=f77cc8251e804a90b14da232308c88fd&zip=' + params.zip;
-   return Ember.$.getJSON(url);
+
+export default Ember.Route.extend({
+ model: function(params) {
+   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=f77cc8251e804a90b14da232308c88fd&zip=' + params.zip;
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     return responseJSON.results;
+   });
   }
 });
